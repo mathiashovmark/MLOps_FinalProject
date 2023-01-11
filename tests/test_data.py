668759f078e922e052e506a83ba8e2e676b0020c
mathiashovmark <mathiashovmark@hotmail.com>
2023-01-11 14:17:37 +0100

    modified:   tests/test_data.py 	deleted:    data.py 	deleted:    main.py 	deleted:    main_model.py 	deleted:    model.py 	modified:   requirements.txt 	.coverage 	__pycache__/ 	data/ 	dockerfile 	gcp_vm_tester.dockerfile 	sample.json 	setup.py 	src/ 	trained_model.pt 	trainer.dockerfile.txt

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,6 @@
 from tests import _PATH_DATA
 _PATH_DATA
-from data import mnist
+from src.data import mnist
 import numpy as np
 import os
 import pytest
@@ -25,8 +25,8 @@ def labelTest():
     for i in range(10):
         assert i in np.unique(train['labels']), "Dataset did not have the correct number of targets"
 
-a=10
-b="7"
-@pytest.mark.parametrize("test_input, expected", [(b, a), ("2+4", 6), ("6*9", 42)])
-def test_eval(test_input, expected):
-    assert eval(test_input) == expected
+#a=10
+#b="7"
+#@pytest.mark.parametrize("test_input, expected", [(b, a), ("2+4", 6), ("6*9", 42)]) # Første og andet argument kan være variable
+#def test_eval(test_input, expected):
+#    assert eval(test_input) == expected
